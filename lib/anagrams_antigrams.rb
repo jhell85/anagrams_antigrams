f503ffe3becd43139c2d81e0fe8cfbdbbd1eb86c
jhell85 <jhell85@gmail.com>
2020-05-08 10:49:22 -0700

    refactored is_english to take in an argument

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,12 +1,12 @@
 require 'whatlanguage'
 
 class AnagramAntigram 
-  attr_reader(:word)
-  def initialize(word)
-    @word = word
+  attr_reader(:word1)
+  def initialize(word1)
+    @word1 = word1
   end
 
-  def is_english?
+  def is_english?(word)
     english = false
     wl = WhatLanguage.new(:english)
     if wl.process_text(word) == {:english=>1}
@@ -14,6 +14,11 @@ class AnagramAntigram
     end
     english
   end
+
+  # def is_antigram
+  #   antigram = false
+
+  # end
 
 
 end
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -6,12 +6,12 @@ describe('Anagram#is_english') do
 
   it ("returns true if word is english") do
     word = AnagramAntigram.new("tomorrow")
-    expect(word.is_english?).to(eq(true))
+    expect(word.is_english?(word.word1)).to(eq(true))
   end
 
   it ("returns false if a word isn't english") do
     word = AnagramAntigram.new("manana")
-    expect(word.is_english?).to(eq(false))
+    expect(word.is_english?(word.word1)).to(eq(false))
   end
 
 end
