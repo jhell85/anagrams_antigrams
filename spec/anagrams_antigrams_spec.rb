c6bb46ebd8f36d574d11295c1645da064c15a8e3
jhell85 <jhell85@gmail.com>
2020-05-08 09:59:55 -0700

    added second test for is_english? method

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -1,11 +1,17 @@
-require ('pry')
-require ('rspec')
-require ('anagrams_antigrams')
+require 'pry'
+require 'rspec'
+require 'anagrams_antigrams'
 
 describe('Anagram#is_english') do 
 
-  it ("returns true false if word is english or not") do
-    word = Anagram.new("today")
+  it ("returns true if word is english") do
+    word = AnagramAntigram.new("tomorrow")
     expect(word.is_english?).to(eq(true))
   end
+
+  it ("returns false if a word isn't english") do
+    word = AnagramAntigram.new("manana")
+    expect(word.is_english?).to(eq(false))
+  end
+
 end
