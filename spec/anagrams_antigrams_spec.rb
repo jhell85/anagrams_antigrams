bafe0f7330e8ab5758117bc6935ed5c021cad9f5
jhell85 <jhell85@gmail.com>
2020-05-08 12:21:34 -0700

    added test to check if word wasn't english in a given sentence

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -3,7 +3,6 @@ require 'rspec'
 require 'anagrams_antigrams'
 
 describe('Anagram#is_english') do 
-
   it ("returns true if word is english") do
     word = AnagramAntigram.new("tomorrow")
     expect(word.is_english?(word.word1)).to(eq(true))
@@ -13,5 +12,16 @@ describe('Anagram#is_english') do
     word = AnagramAntigram.new("manana")
     expect(word.is_english?(word.word1)).to(eq(false))
   end
+end
 
+describe('AnagramAntigram#check_phrase') do
+  it ("returns an empty Array for an english phrase") do
+    word = AnagramAntigram.new("Hello my friend how are you today")
+    expect(word.check_phrase(word.word1)).to(eq(Array.new()))
+  end
+
+  it ("returns an empty Array for an english phrase") do
+    word = AnagramAntigram.new("Hello my frnd how are you today")
+    expect(word.check_phrase(word.word1)).to(eq(["frnd"]))
+  end
 end
